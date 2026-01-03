cebb011b53e74666f157b3d9dcf275483ae0c18f
Alex Roth <alex@alexroth.me>
2026-01-03 16:18:20 -0500

    Added embassy-executor (3K overhead)

--- firmware/README.md
+++ firmware/README.md
@@ -1,1 +1,4 @@
 there are examples within embassy for this MCU - we're clearly building something just slightly wrong.
+
+-> Get executor working.
+-> Get UART working.
--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -2,8 +2,8 @@
 #![no_std]
 
 use cortex_m::{asm::nop};
-use embassy_mspm0::{Config, gpio::Output};
-
+use embassy_mspm0::{Config, gpio::Output, uart::Uart};
+use embassy_executor::Spawner;
 
 use embassy_time::{Delay, Duration};
 use embedded_hal::delay::DelayNs;
@@ -11,8 +11,8 @@ use embedded_hal::delay::DelayNs;
 // TODO(5) adjust HAL import
 // use some_hal as _; // memory layout
 
-use panic_probe as _;
 
+use panic_probe as _;
 /// Terminates the application and makes a semihosting-capable debug tool exit
 /// with status code 0.
 pub fn exit() -> ! {
@@ -29,34 +29,26 @@ unsafe fn HardFault(_frame: &cortex_m_rt::ExceptionFrame) -> ! {
     semihosting::process::exit(1);
 }
 
-#[cortex_m_rt::entry]
-fn main() -> ! {
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) -> ! {
     let periph = embassy_mspm0::init(Default::default());
 
     let mut led_output = Output::new(periph.PA22, embassy_mspm0::gpio::Level::Low);
+    
+    let tx = periph.PA27;
+    let rx = periph.PA26;
 
-    let mut cnt = 0;
-    while cnt < 100 {
-        cnt = cnt + 1;
-        nop();
-    }
+    let mut config = embassy_mspm0::uart::Config::default();
+    config.baudrate = 115200;
+
+    let mut uart = Uart::new_blocking(periph.UART0, rx, tx, config).unwrap();
+
+
     loop {
         led_output.toggle();
-        Delay.delay_ms(100);
-    }
-}
 
-
-// defmt-test 0.3.0 has the limitation that this `#[tests]` attribute can only be used
-// once within a crate. the module can be in any file but there can only be at most
-// one `#[tests]` module in this library crate
-#[cfg(test)]
-#[defmt_test::tests]
-mod unit_tests {
-    use defmt::assert;
-
-    #[test]
-    fn it_works() {
-        assert!(true)
+        uart.blocking_write(b"abcd1334\r\n").unwrap();
+        
+        Delay.delay_ms(1000);
     }
 }
